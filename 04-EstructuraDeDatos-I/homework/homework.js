54c4cb07533ddc752727eeec3d68552bd975f37a
linaroyet <linasineci@gmail.com>
2023-07-25 10:44:13 -0500

    Ejercicios Resueltos

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,35 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if(n<0) return "No se admiten numeros negativos"
 
-function nFibonacci(n) {}
+  if (n == 0 || n == 1) { // 
+    return 1; // si la condicion anterior se cumple retornar un 1
+  }
+  else { // si la condicion anterior no se cumple retornar 
+    return n * nFactorial(n - 1);  // 5 (5-1)  => 4
+                                   // 4 (4-1)  => 3
+                                   // 3 (3-1)  => 2
+                                   // 2 (2-1)  => 1
+                                   // 1 =>       1
+  }
+}
+console.log(nFactorial(8)); 
+
+
+function nFibonacci(n) {
+  if (n === 0 || n===1) { // Si en caso de que el numero ingresado sea cero retorne 0
+    return n; 
+  }
+
+     // if(n===0 || n===1) return n; 
+  }
+  if (n >= 2) { // Segun la ecuacion de fibonacci inicia en 2
+    return nFibonacci(n - 2) + nFibonacci(n - 1) // ecuacion de fibocanni, donde se realioza dos operaciones. 
+  }
+// console.log(nFibonacci(5));
+
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +53,27 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+function Queue() {
+  this.array = []; // se crea un array para guardar la información de los metodos 
+}
+Queue.prototype.enqueue = function (valor) { // se crea un metodo enqueue donde se hereda el arreglo que se encuentra en la funcion queue
+  return this.array.push(valor); // retornamos el arreglo agregandole un valo al final del arreglo
+}
+
+Queue.prototype.dequeue = function () { // se crea un metodo llamado dequeue donde se hereda el arreglo que se encuentra en la funcion Queue
+  if (this.array.length === 0) { // se crea un condicional donde si la lñongitud del arreglo es 0 retorne undefined
+    return undefined;
+  } else { // si la condiciop anterior no se cumple  retorne el arreglo sin el ultimo elemento 
+    return this.array.shift();
+  }
+
+}
+
+Queue.prototype.size = function () { // se crea un metodo llamado Syze donde se retorne la longitud del arreglo. 
+  return this.array.length;
+}
+
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
