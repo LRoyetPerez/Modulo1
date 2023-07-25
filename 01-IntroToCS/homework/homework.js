ad4ca274f339f2a5dd5200de992ec49429a2b20d
linaroyet <linasineci@gmail.com>
2023-07-25 10:25:16 -0500

    Ejercicio Resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,29 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   vjdfifhfhfiijdisjiids
+   var exponente = num.length - 1;
+   var acumulador = 0;
+   var i;
+   for (i = 0; i < num.length; i++) {
+      acumulador = acumulador + (num[i] * Math.pow(2, exponente));
+      exponente = exponente - 1;
+   }
+   return acumulador;
 }
+console.log(BinarioADecimal("1111"));
 
-function DecimalABinario(num) {}
+
+function DecimalABinario(num) { 
+   var resultado= ""; 
+   while(num>0){
+     resultado=num%2 + resultado; 
+     num=Math.floor(num/2); 
+   }
+   return resultado; 
+
+  }
+  console.log(DecimalABinario(2)); 
+
 
 module.exports = {
    BinarioADecimal,
